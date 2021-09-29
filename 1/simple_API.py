ef58c93cd2f846a0c354f894f666f5f9530f83e5
JASG94 <javisalvador94@gmail.com>
2021-09-29 18:43:48 +0200

    Modified simple_API

--- 1/simple_API.py
+++ 1/simple_API.py
@@ -7,12 +7,14 @@ returned_data = {"hello" : "world"}
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Only avilable endopoint is /helloworld"
+    return "Only avilable endpoint is /helloworld"
 
 @app.route('/helloworld', methods=['GET'])
 def api_all():
     return flask.jsonify(returned_data)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
 
 app.run()
